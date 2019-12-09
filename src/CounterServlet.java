3f43c576d58446325819eb03bd5077710c2c426d
Piotr Komosa <komosa.piotr@gmail.com>
2019-12-09 19:03:20 +0100

    commit2

--- src/CounterServlet.java
+++ src/CounterServlet.java
@@ -9,35 +9,88 @@ import java.io.IOException;
 public class CounterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        countLengthUnit(request, response);
         String metry = request.getParameter("metry");
         String centymetry = request.getParameter("centymetry");
         String milimetry = request.getParameter("milimetry");
-        String Przelicz = request.getParameter("Przelicz");
-        double meters;
-        double centimeters;
-        double milimeters;
-        meters = Double.parseDouble(metry);
-        centimeters = Double.parseDouble(centymetry);
-        milimeters = Double.parseDouble(milimetry);
+//        double meters;
+//        double centimeters;
+//        double milimeters;
+
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
-        if (metry != null && centymetry.isEmpty() && milimetry.isEmpty() ) {
-            response.getWriter().print("Podana wartość w przeliczeniu na: ");
-            response.getWriter().print("metry: " + meters);
-            response.getWriter().print("centymetry: " + meters / 100);
-            response.getWriter().print("milimetry: " + meters / 1000);
-        } else if (metry.isEmpty() && milimetry.isEmpty()) {
-            response.getWriter().print("Podana wartość w przeliczeniu na: ");
-            response.getWriter().print("metry: " + centimeters / 100);
-            response.getWriter().print("centymetry: " + centimeters);
-            response.getWriter().print("milimetry: " + centimeters * 10);
-        } else if (metry.isEmpty() && centymetry.isEmpty()) {
-            response.getWriter().print("Podana wartość w przeliczeniu na: ");
-            response.getWriter().print("metry: " + milimeters / 100);
-            response.getWriter().print("centymetry: " + milimeters);
-            response.getWriter().print("milimetry: " + milimeters * 10);
+    }
+
+    private void countLengthUnit(HttpServletRequest request, HttpServletResponse response) {
+
+        Double meters = (Double) request.getAttribute("metry");
+        Double centimeters = (Double) request.getAttribute("centymetry");
+        Double milimeters = (Double) request.getAttribute("milimetry");
+        if (checkIfValuesCorrect(meters, centimeters, milimeters)) {
+            if (meters != null) {
+                centimeters = meters * 100;
+                milimeters = meters * 1000;
+            } else if (centimeters != null) {
+                meters = centimeters / 100;
+                milimeters = centimeters * 10;
+            } else if (milimeters != null) {
+                centimeters = milimeters / 10;
+                meters = milimeters / 1000;
+            }
+            request.setAttribute("metry", meters);
+            request.setAttribute("centymetry", centimeters);
+            request.setAttribute("milimetry", milimeters);
+        } else {
+            System.out.println("error");
+
+        }
+
+    }
+    private boolean checkIfValuesCorrect(Double meters, Double centimeters, Double milimeters) {
+        if (meters != null) {
+            if (centimeters != null) {
+                return false;
+            } else {
+                if (milimeters != null) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        } else {
+            if (centimeters != null) {
+                if (milimeters != null) {
+                    return false;
+                } else {
+                    return true; }
+                else {
+                    if (milimeters != null) {
+                        return true;
+                    }
+                }
+            }
         }
     }
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    //
+//
+//        if (centymetry.isEmpty() && milimetry.isEmpty() ) {
+//            response.getWriter().print("Podana wartość w przeliczeniu na: ");
+//            response.getWriter().print("metry: " + meters);
+//            response.getWriter().print("centymetry: " + meters / 100);
+//            response.getWriter().print("milimetry: " + meters / 1000);
+//        } else if (metry.isEmpty() && milimetry.isEmpty()) {
+//            response.getWriter().print("Podana wartość w przeliczeniu na: ");
+//            response.getWriter().print("metry: " + centimeters / 100);
+//            response.getWriter().print("centymetry: " + centimeters);
+//            response.getWriter().print("milimetry: " + centimeters * 10);
+//        } else if (metry.isEmpty() && centymetry.isEmpty()) {
+//            response.getWriter().print("Podana wartość w przeliczeniu na: ");
+//            response.getWriter().print("metry: " + milimeters / 100);
+//            response.getWriter().print("centymetry: " + milimeters);
+//            response.getWriter().print("milimetry: " + milimeters * 10);
+//        }
+//    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException {
     }
 }
